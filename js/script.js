183878aadb10f4b03a1ea186b1da84064a9562a5
vijoh01 <76432491+vijoh01@users.noreply.github.com>
2022-12-16 01:57:04 +0100

    Färdig!!

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
 $(() => {
-    $('#search button').click((e) => {
-        $.fn.loading(true);
-
+    
+    $('.search button').click((e) => {
         e.preventDefault();
+        if ($('.search-input').val().length == 0) {
+            $.fn.errorMsg("Enter search input");
+            return;
+        }
+        $.fn.errorMsg("");
+
         $('.search-container').html("");
-        fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9feba22bfaebe544497d5cea5fae9664&tags=${$('#search-input').val()}&sort=${$('#sort').val()}&per_page=${$('#display-amount').val()}&format=json&nojsoncallback=1`)
-            .then((response) => response.json())
+        fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9feba22bfaebe544497d5cea5fae9664&tags=${$('.search-input').val()}&sort=${$('.sort').val()}&per_page=${$('.display-amount').val()}&format=json&nojsoncallback=1`)
+            .then((response) => {
+                if (response.status >= 200 && response.status < 300 && response.ok == true) {
+                    return response.json();
+                } else {
+                    throw $.fn.errorMsg("Could not get response from api, try again later.");
+
+                }
+            })
             .then((data) => {
                 $.fn.loading(false);
                 let imgContainer = document.createElement('div');
                 let column = document.createElement('div');
 
                 let photos = data.photos.photo;
+                if (photos.length == 0) {
+                    return $.fn.errorMsg("Could not find images containing '" + $('.search-input').val() + "'");
+                }
+
                 photos.forEach((element, index) => {
                     let img = document.createElement('img');
-                    
-                    img.src = `https://live.staticflickr.com/${element.server}/${element.id}_${element.secret}_${$('#img-size').val()}.jpg`;
+
+                    img.src = `https://live.staticflickr.com/${element.server}/${element.id}_${element.secret}_${$('.img-size').val()}.jpg`;
                     img.className = "list-img";
                     $(img).attr('draggable', 'false');
                     column.className = "column";
@@ -29,14 +45,12 @@ $(() => {
                     imgContainer.className = "img-container";
                     $('.search-container').append(imgContainer);
                 });
-        
+
                 $('.list-img').hover((e) => {
-                    
                     const target = $(e.target).parent();
                     if (e.target instanceof Image) {
                         if (!$(target).data().selected === 0) return;
-                        e.target.animate({filter: 'blur(6px)' }, 150, target.css('z-index', 101));
-                        
+                        e.target.animate({ filter: 'blur(6px)' }, 150, target.css('z-index', 101));
                     }
                 }, () => $('.column').css('z-index', 1));
 
@@ -47,29 +61,42 @@ $(() => {
                         $('.img-popup-container').css('display', 'none');
                     }
                 });
-                
-                $('img').click((e) => { 
-                    if (!$('.img-popup-container').is(e.target) && !$('.img-popup-container').has(e.target).length) 
-                    e.target.animate({transform: 'scale(1.1)', filter: 'blur(6px)' }, 150, $(e.target).css('z-index', 101));
+
+                $('img').click((e) => {
+                    if (!$('.img-popup-container').is(e.target) && !$('.img-popup-container').has(e.target).length)
+                        e.target.animate({ transform: 'scale(1.1)', filter: 'blur(6px)' }, 150, $(e.target).css('z-index', 101));
                     $('.img-popup-container').css('display', 'block');
                     $('.img-display').attr("src", $(e.target).attr('src'));
-                    
+
                 });
+            }).catch(() => {
+                throw $.fn.errorMsg("Could not get response from api, try again later.");
             });
     });
+
+    $.fn.errorMsg = (msg) => {
+        if (msg.length == 0) {
+            $('.search-msg').css('visibility', 'hidden');
+            return $.fn.loading(true);
+        }
+        $.fn.loading(false);
+        $('.search-container').empty();
+        $('.search-msg').css('visibility', 'visible');
+        $('.search-msg').text(msg);
+    }
+
+    let times = 0;
 
     $.fn.loading = (show) => {
         if (!show) {
             $('.loading-container').css('visibility', 'hidden');
             return;
         }
-        $('.loading-container').css('display', 'block');
         $('.loading-container').css('visibility', 'visible');
-        let times = 0;
-        setInterval(function() {
-            times++;
-        $('.loading').animate({rotate: times + '360deg'}, {duration: 1500});
-        }, 100);
-    }
 
+        setInterval(function () {
+            times++;
+            $('.loading').animate({ loop: true, rotate: times + '360deg' }, { duration: 1500, loop: true });
+        }, 150);
+    }
 });
